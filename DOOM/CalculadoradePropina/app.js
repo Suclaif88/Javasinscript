6027d87b900e032531b35bcbbbc549864122c1f6
Mona Lisa <roldansebasdaza@gmail.com>
2024-05-17 22:45:00 -0500

    17/05/2024

--- DOOM/CalculadoradePropina/app.js
+++ DOOM/CalculadoradePropina/app.js
@@ -3,4 +3,10 @@ function calcularPropina(){
     let propina = document.querySelector('input[name="propina"]:checked').value;
     let resultado = total * propina;
     console.log(resultado)
+
+    Swal.fire({
+        title: "Propina Calculada",
+        text: resultado,
+        icon: "info"
+      });
 }
