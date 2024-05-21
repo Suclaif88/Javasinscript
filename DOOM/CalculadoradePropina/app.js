a16947a34eea2e91b3e50710874cfd62d75b84e8
MONA-LISA <roldansebasdaza@gmail.com>
2024-05-21 16:20:56 -0500

    21/05/2024

--- DOOM/CalculadoradePropina/app.js
+++ DOOM/CalculadoradePropina/app.js
@@ -2,7 +2,7 @@ function calcularPropina(){
     let total = document.getElementById('total').value;
     let propina = document.querySelector('input[name="propina"]:checked').value;
     let resultado = total * propina;
-    console.log(resultado)
+    // console.log(resultado)
 
     Swal.fire({
         title: "Propina Calculada",
