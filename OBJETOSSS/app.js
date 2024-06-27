0299d67e361d0ff0135197bdd45712b92e51f7c3
Mona Lisa <roldansebasdaza@gmail.com>
2024-06-26 22:09:05 -0500

    10/07/2024

---
+++ OBJETOSSS/app.js
@@ -0,0 +1,23 @@
+var prompt = require('prompt-sync')();
+
+var Eleccion = prompt('¿QUE AUTO DESEA COMPRAR?: ');
+
+console.log(Eleccion)
+
+// const CARRO = {
+//     MARCA : "AUDI",
+//     MODELO : "R8",
+//     COLOR : "BLANCO",
+//     PLACA : "ABC123",
+//     TRANSMICION : "MANUAL",
+//     PUERTAS : '4',
+//     ANIO : '2020'
+// }
+
+// console.log(CARRO)
+
+
+
+
+
+
